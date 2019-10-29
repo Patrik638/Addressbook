718ae25d5368285fcfba2f5f3f674f8ebe8d69b8
Patrik.Karlsson <Patrik.Karlsson@4PBRXT2.autocom.local>
2019-10-29 18:28:30 +0100

    Updated class Person.java

--- src/main/java/com/grupp2/addressbook/Person.java
+++ src/main/java/com/grupp2/addressbook/Person.java
@@ -1,14 +1,79 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.grupp2.addressbook;
 
-/**
- *
- * @author Patrik.Karlsson
- */
-public class Person {
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "addressbook")
+public class Person implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "firstname")
+    private String firstName;
+
+    @Column(name = "lastname")
+    private String lastName;
+
+    @Column(name = "phonenumber")
+    private String phoneNumber;
+    
+    @Column(name = "email")
+    private String eMail;
+    
+    public Person() {}
+
+    public Person(int id, String firstName, String lastName,
+              String phoneNumber, String eMail) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.eMail = eMail;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+    public String getEMail() {
+        return eMail;
+    }
+    public void setEMail(String eMail) {
+        this.eMail = eMail;
+    }
     
 }
