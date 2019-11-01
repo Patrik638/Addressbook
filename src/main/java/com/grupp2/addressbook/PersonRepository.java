256d246fea15ff22c3871b15fc80be83fe25b2a4
Patrik.Karlsson <Patrik.Karlsson@4PBRXT2.autocom.local>
2019-11-01 13:25:34 +0100

    Changed Method findByfirstName Added Method findBylastName

--- src/main/java/com/grupp2/addressbook/PersonRepository.java
+++ src/main/java/com/grupp2/addressbook/PersonRepository.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
-    List<Person> findByfirstName(String name);
+    
+    List<Person> findByfirstName(String firstName);
+    List<Person> findBylastName(String lastName);
 
     @Query(value = "SELECT p FROM Person p LEFT JOIN Status s ON p.statId = s.id"
               + " WHERE s.actualStatus = 'active' ORDER BY p.lastName, p.firstName")
@@ -17,4 +19,5 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
     @Query(value = "SELECT p FROM Person p LEFT JOIN Status s ON p.statId = s.id"
               + " WHERE s.actualStatus = 'inactive' ORDER BY p.lastName, p.firstName")
     List<Person> findAllDeletedPersons();
+    
 }
